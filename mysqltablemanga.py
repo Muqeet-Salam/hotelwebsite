74928d7dfb7f1cd78bb6640b4fb7ff9541d57e53
Mohammed Muqeet Us Salam <87235900+Muqeet-Salam@users.noreply.github.com>
2024-08-07 18:35:46 +0530

    Delete mysqltablemanga.py

--- mysqltablemanga.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect(host = "localhost", user = 'nawb', password= 'bk201phile', database = 'nawb')
-mycursor = mydb.cursor()
-mycursor.execute("select * from manga")
-result = mycursor.fetchall()
-for i in result:
-    print(i)
